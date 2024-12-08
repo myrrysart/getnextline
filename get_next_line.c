321eafad3391abecb0821b264e200ad0ddf77344
myrrysart <hello@myrrys.art>
2024-12-08 20:54:23 +0200

    wip(getnextline): some initial plans for how to fill and get proper line stats to printer function

--- get_next_line.c
+++ get_next_line.c
@@ -12,26 +12,43 @@
 
 #include "get_next_line.h"
 
+size_t	ft_strlen(char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*(s++))
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(char *buf, size_t len)
+{
+	return (NULL);
+}
+
 int	fill_node(char *buf)
 {
 	t_strholder	new;
 
-	new.str->buf;
-	new.next->NULL;
+	new.str = ft_strdup(buf, ft_strlen(buf) + 1);
+	new.next = NULL;
+	return (0);
 }
 
-char	*get_next_line(int fd)
-{
-	char	*buf;
+//char	*get_next_line(int fd)
+//{
+//	char	*buf;
+//
+//	buf = malloc(BUFFER_SIZE + 1);
+//	if (!buf)
+//		return (NULL);
+//	read(open(argv[1], O_RDONLY), buf, READ_SIZE);
+//	if (fill_node(buf))
+//		return (NULL);
+//	return (buf);
+//}
 
-	buf = malloc(BUFFER_SIZE + 1);
-	if (!buf)
-		return (NULL);
-	read(open(argv[1], O_RDONLY), buf, READ_SIZE);
-	if (fill_node(buf))
-		return (NULL);
-	return (buf);
-}
 
 int	main(int argc, char **argv)
 {
