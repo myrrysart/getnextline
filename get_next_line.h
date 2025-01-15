77c2d0555dcb7fc44e4df52ca889dbf63dbd1d6e
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-01-15 16:30:38 +0200

    feat(header): definitions for read_size and buffer_size added

--- get_next_line.h
+++ get_next_line.h
@@ -12,10 +12,13 @@
 
 #ifndef    GET_NEXT_LINE_H
 # define    GET_NEXT_LINE_H
-# define    READ_SIZE 5
+
+# ifndef    READ_SIZE
+#  define    READ_SIZE 8
+# endif
 
 # ifndef    BUFFER_SIZE
-#  define    BUFFER_SIZE 10
+#  define    BUFFER_SIZE 8
 # endif
 
 # include <stdio.h> //test
