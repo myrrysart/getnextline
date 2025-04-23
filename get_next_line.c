9cb7bfd3ea55495f8dccc58e0cb18fff11e176b7
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-04-23 17:37:25 +0300

    refactor+fix: extract_line now takes care of all possible situations

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 14:40:00 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/04/23 12:27:34 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/04/23 17:28:36 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,45 @@ static void	safe_free(void **ptr)
 	*ptr = NULL;
 }
 
-static char	*extract_line(char **remains)
+static int	process_newline(char **remains, char **line, char *newline_pos)
 {
-	char	*line;
-	char	*newline_pos;
 	char	*temp;
 
-	if (!*remains || !**remains)
+	*line = ft_substr(*remains, 0, newline_pos - *remains + 1);
+	if (!*line)
+		return (0);
+	if (*(newline_pos + 1))
 	{
-		safe_free((void *)remains);
-		return (NULL);
-	}
-	newline_pos = ft_strchr(*remains, '\n');
-	if (newline_pos)
-	{
-		line = ft_substr(*remains, 0, newline_pos - *remains + 1);
 		temp = ft_strdup(newline_pos + 1);
+		if (!temp)
+		{
+			safe_free((void *)line);
+			return (0);
+		}
 		safe_free((void *)remains);
 		*remains = temp;
 	}
 	else
+		safe_free((void *)remains);
+	return (1);
+}
+
+static char	*extract_line(char **remains)
+{
+	char	*line;
+	char	*newline_pos;
+
+	if (!remains || !*remains)
+		return (NULL);
+	newline_pos = ft_strchr(*remains, '\n');
+	if (!newline_pos)
 	{
 		line = ft_strdup(*remains);
 		safe_free((void *)remains);
+		return (line);
 	}
+	if (!process_newline(remains, &line, newline_pos))
+		return (NULL);
 	return (line);
 }
 
@@ -52,6 +67,7 @@ static int	read_to_remains(char **remains, int fd)
 	int		bytes_read;
 	int		continue_reading;
 	char	*buffer;
+	char	*temp;
 
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
@@ -61,13 +77,14 @@ static int	read_to_remains(char **remains, int fd)
 	while (bytes_read > 0 && continue_reading)
 	{
 		buffer[bytes_read] = '\0';
-		*remains = ft_strjoin(*remains, buffer);
-		if (!*remains)
+		temp = ft_strjoin(*remains, buffer);
+		if (!temp)
 			return (-1);
-		if (ft_strchr(*remains, '\n'))
+		if (ft_strchr(temp, '\n'))
 			continue_reading = 0;
 		else
 			bytes_read = read(fd, buffer, BUFFER_SIZE);
+		*remains = temp;
 	}
 	safe_free((void *)&buffer);
 	return (bytes_read);
@@ -80,8 +97,6 @@ char	*get_next_line(int fd)
 	int			bytes_read;
 
 	bytes_read = 0;
-	if (!remains)
-		remains = ft_strdup("");
 	bytes_read = read_to_remains(&remains, fd);
 	if (bytes_read < 0)
 		safe_free((void *)&remains);
