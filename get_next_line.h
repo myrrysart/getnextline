5d6775c9d59888aa5aee8c3e4d3c706e7b0e5592
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-04-23 17:40:03 +0300

    rename:ft_strjoin is not descriptive name, welcome concatonation!

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 14:53:53 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/04/22 21:37:38 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/04/23 15:05:03 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char *s1, char *s2);
+char	*concat_buffer_to_remains(char *remains, char *buffer);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
